8a083e0863cc38a0e02e57d03b5127e8a29aa903
Jayesh Gaba <jg9140@srmist.edu.in>
2024-08-05 22:01:16 +0530

    shodan implemented

--- info_gatherer.py
+++ info_gatherer.py
@@ -15,8 +15,18 @@ args = parser.parse_args()
 domain = args.domain
 ip = args.shodan
 
-print(f"Gathering information for domain: {domain} & IP: {ip}")
-
+if not ip:
+    response = input("Do you want to perform a shodan search on the given domain? [Y/N]: ").strip().lower()
+    if response == 'y':
+        ip = socket.gethostbyname(domain)
+    else:
+        pass
+    
+if ip:
+    print(f"Gathering information for domain: {domain} & IP: {ip}")
+else:
+    print(f"Gathering information for domain: {domain}")
+          
 print("Gathering WHOIS info... ")
 try:
     d = whois.whois(domain)
@@ -38,25 +48,25 @@ print("[+] Gathering DNS info...")
 
 try:
     for a in dns.resolver.resolve(domain, 'A'):
-        print(f"[+] A Record: {a.to_text()}")
+        print(f"[*] A Record: {a.to_text()}")
 except:
     print("[-] Could not extract A Records!")
 
 try:
     for ns in dns.resolver.resolve(domain, 'NS'):
-        print(f"[+] NS Record: {ns.to_text()}")
+        print(f"[*] NS Record: {ns.to_text()}")
 except:
     print("[-] Could not extract NS Records!")
 
 try:
     for mx in dns.resolver.resolve(domain, 'MX'):
-        print(f"[+] MX Record: {mx.to_text()}")
+        print(f"[*] MX Record: {mx.to_text()}")
 except:
     print("[-] Could not extract MX Records!")
 
 try:
     for t in dns.resolver.resolve(domain, 'TXT'):
-        print(f"[+] TXT Record: {t.to_text()}")
+        print(f"[*] TXT Record: {t.to_text()}")
 except:
     print("[-] Could not extract TXT Records!")
     
@@ -74,10 +84,32 @@ except:
     
 
 if ip:
+    print(f"[+] Gathering info from Shodan for IP: {ip}...")
     load_dotenv()
     key = os.getenv('SHODAN_API_KEY')
+    api = shodan.Shodan(key)
+    
     try:
-        results = key.search(ip)
-        print(f"[+] {results['total']} results found!")
-    except:
-        pass
+        results = api.host(ip)
+        number_of_results = len(results.get('data', []))
+        print(f"[+] Number of different results found: {number_of_results}")
+
+        print(f"\n[+] IP: {results.get('ip_str', 'N/A')}")
+        print(f"[*] Organization: {results.get('org', 'N/A')}")
+        print(f"[*] ISP: {results.get('isp', 'N/A')}")
+        print(f"[*] Country: {results.get('country_name', 'N/A')}")
+        print(f"[*] City: {results.get('city', 'N/A')}")
+        print(f"[*] Ports: {results.get('ports', [])}")
+
+        for index, result in enumerate(results.get('data', []), start=1):
+            print(f"\n[+] Result {index}:")
+            print(f"IP: {result.get('ip_str', 'N/A')}")
+            print(f"Port: {result.get('port', 'N/A')}")
+            print(f"Data: \n{result.get('data', 'N/A')}")
+            print(f"Hostnames: {result.get('hostnames', [])}")
+            print(f"Location: {result.get('location', {})}")
+            print("-" * 150)
+
+    except Exception as e:
+        print("[-] Shodan search error!")
+        print(e)
